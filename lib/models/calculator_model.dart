05c38ecb93f13ec3bf8815fdafc99e98876c008c
MITPIT <mitoopito@gmail.com>
2026-01-12 08:26:16 +0000

    Moved project files to root for easier launching

---
+++ lib/models/calculator_model.dart
@@ -0,0 +1,9 @@
+class CalculatorModel {
+  double add(double a, double b) => a + b;
+  double subtract(double a, double b) => a - b;
+  double multiply(double a, double b) => a * b;
+  double divide(double a, double b) {
+    if (b == 0) throw Exception("Division by zero");
+    return a / b;
+  }
+}
